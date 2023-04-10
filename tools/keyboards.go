3d16121104017aa686fac0b68c626fdcb7c58a31
aliforever <alipadida@live.com>
2023-04-10 11:29:42 +0300

    added a keyboard helper method

--- tools/keyboards.go
+++ tools/keyboards.go
@@ -16,3 +16,25 @@ func (k Keyboards) NewInlineKeyboardFromSlicesOfMaps(slicesOfMaps [][]map[string
 func (k Keyboards) NewReplyKeyboardFromSlicesOfStrings(slicesOfStrings [][]string) *structs.ReplyKeyboardMarkup {
 	return (&structs.ReplyKeyboardMarkup{}).FromSlicesOfStrings(slicesOfStrings).SetResizeKeyboard(true)
 }
+
+// NewReplyKeyboardFromSliceOfStrings creates a new ReplyKeyboardMarkup from a slice of strings.
+// The slice of strings will be divided into slices of strings with the length of bpr (buttons per row).
+func (k Keyboards) NewReplyKeyboardFromSliceOfStrings(sliceOfStrings []string, bpr int) *structs.ReplyKeyboardMarkup {
+	var rows [][]string
+	var row []string
+
+	for _, s := range sliceOfStrings {
+		row = append(row, s)
+
+		if len(row) >= bpr {
+			rows = append(rows, row)
+			row = []string{}
+		}
+	}
+
+	if len(row) > 0 {
+		rows = append(rows, row)
+	}
+
+	return (&structs.ReplyKeyboardMarkup{}).FromSlicesOfStrings(rows).SetResizeKeyboard(true)
+}
