6af3196454b1f11c5ca81e4317a5ff6a48673c8e
aliforever <alipadida@live.com>
2023-04-20 23:42:27 +0300

    added helpers for keyboards

--- tools/keyboards.go
+++ tools/keyboards.go
@@ -13,6 +13,24 @@ func (k Keyboards) NewInlineKeyboardFromSlicesOfMaps(slicesOfMaps [][]map[string
 	return (&structs.InlineKeyboardMarkup{}).FromSlicesOfMaps(slicesOfMaps)
 }
 
+func (k Keyboards) NewInlineKeyboardFromSlicesOfMapWithFormation(
+	slicesOfMaps []map[string]string, bpr int, maxPerRow int, formation []int) *structs.InlineKeyboardMarkup {
+
+	var rows [][]map[string]string
+	var row []map[string]string
+
+	for _, s := range slicesOfMaps {
+		row = append(row, s)
+
+		if k.shouldBreakRow(len(row), len(rows), bpr, maxPerRow, formation) {
+			rows = append(rows, row)
+			row = []map[string]string{}
+		}
+	}
+
+	return (&structs.InlineKeyboardMarkup{}).FromSlicesOfMaps(rows)
+}
+
 func (k Keyboards) NewReplyKeyboardFromSlicesOfStrings(slicesOfStrings [][]string) *structs.ReplyKeyboardMarkup {
 	return (&structs.ReplyKeyboardMarkup{}).FromSlicesOfStrings(slicesOfStrings).SetResizeKeyboard(true)
 }
@@ -37,4 +55,45 @@ func (k Keyboards) NewReplyKeyboardFromSliceOfStrings(sliceOfStrings []string, b
 	}
 
 	return (&structs.ReplyKeyboardMarkup{}).FromSlicesOfStrings(rows).SetResizeKeyboard(true)
+}
+
+// NewReplyKeyboardFromSliceOfStringsWithFormation creates a new ReplyKeyboardMarkup from a slice of strings.
+// The slice of strings will be divided into slices of strings with the length of bpr (buttons per row).
+// The formation of the keyboard is defined by the slice of integers.
+func (k Keyboards) NewReplyKeyboardFromSliceOfStringsWithFormation(
+	sliceOfStrings []string, bpr int, buttonFormation []int) *structs.ReplyKeyboardMarkup {
+
+	var rows [][]string
+	var row []string
+
+	for _, s := range sliceOfStrings {
+		row = append(row, s)
+
+		if k.shouldBreakRow(len(row), len(rows), bpr, 0, buttonFormation) {
+			rows = append(rows, row)
+			row = []string{}
+		}
+	}
+
+	if len(row) > 0 {
+		rows = append(rows, row)
+	}
+
+	return (&structs.ReplyKeyboardMarkup{}).FromSlicesOfStrings(rows).SetResizeKeyboard(true)
+}
+
+func (k Keyboards) shouldBreakRow(
+	rowLength int, rowsLength int, defaultBpr int, maxButtonPerRow int, buttonFormation []int) bool {
+
+	defaultCond := rowLength >= defaultBpr
+
+	if maxButtonPerRow > 0 {
+		defaultCond = rowLength >= maxButtonPerRow
+	}
+
+	if len(buttonFormation) > rowsLength {
+		defaultCond = rowLength >= buttonFormation[rowsLength]
+	}
+
+	return defaultCond
 }
