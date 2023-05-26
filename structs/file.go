e431498739ff702d887be4865095b06d9dc77dcd
aliforever <alipadida@live.com>
2023-05-26 15:41:28 +0300

    added download path

--- structs/file.go
+++ structs/file.go
@@ -1,9 +1,16 @@
 package structs
 
+import "fmt"
+
 type File struct {
 	FileId       string             `json:"file_id"`
 	FileUniqueId string             `json:"file_unique_id"`
 	FileSize     int64              `json:"file_size"`
 	FilePath     string             `json:"file_path"`
 	Download     func(string) error `json:"-"`
+}
+
+// DownloadPath returns the path to download the file to.
+func (f *File) DownloadPath(botToken string) string {
+	return fmt.Sprintf("https://api.telegram.org/file/bot%s/%s", botToken, f.FilePath)
 }
